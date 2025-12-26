d8b8a73a1ac722c0a208837e94e5ee994116841b
bwork38 <bworkshmelisr@gmail.com>
2025-12-26 13:59:35 -0800

    Update ALLOWED_HOSTS in settings.py for Render deployment

--- AmazonProject/settings.py
+++ AmazonProject/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ---------------------------------------------------------------------
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+ALLOWED_HOSTS = ['https://myprojectt-kxdt.onrender.com']
 
 # ---------------------------------------------------------------------
 # Application definition

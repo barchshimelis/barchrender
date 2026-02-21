d4cb67af27a3b37405ed82923e46d28e3a44f8b3
barchh <bworkshimelis1738@gmail.com>
2026-02-21 08:48:39 -0500

    Final: Update settings and build.sh for Render deploymenttt

--- AmazonProject/urls.py
+++ AmazonProject/urls.py
@@ -25,5 +25,4 @@ urlpatterns += i18n_patterns(
 )
 
 # Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
